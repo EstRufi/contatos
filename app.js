d539d8197a4eeb8759bd4ebbec2b4edd93ced978
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-05-18 14:20:01 -0300

    enviando o post finalizado

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 'use strict'
 
+import { getContatos, getContato,postContato } from "./contatos.js"
+
+const novoContato ={
+    "nome": "Estela",
+    "celular": "11 9 7171-1264",
+    "foto": "https://img.freepik.com/psd-gratuitas/renderizacao-3d-do-estilo-de-cabelo-para-o-design-do-avatar_23-2151869121.jpg",
+    "email": "estela@gmail.com",
+    "endereco": "Av. São Joaquim, 234",
+    "cidade": "Itapevi"
+}
+
+console.table(await postContato(novoContato))
--- contatos.js
+++ contatos.js
@@ -1,14 +1,35 @@
 'use strict'
+"A única pessoa que nunca erra é a pessoa que não faz nada"
 
 const url = 'https://bakcend-fecaf-render.onrender.com/contatos'
 
-export async function getContato() {
+export async function getContatos() {
     const response = await fetch(url)
 
-    if(response.ok){
-        return response.json
+    if(!response.ok) throw new Error('Erro ao listar um novo contato!')
+        return response.json()
+}
+
+export async function getContato(id){
+    const response = await fetch(`${url}/${id}`)
+
+    if(!response.ok) throw new Error(`Erro ao listar o contato ${id}`)
+        return response.json()
+}
+
+// Aqui aprendemos sobre criar um post
+export async function postContato(contato){
+    // aqui fizemos isso para indicar o fetch oq estamos fazendo
+    const options ={
+        method: 'POST',
+        headers:{
+            "Content-Type":'application/json'
+        },
+        body: JSON.stringify(contato)
     }
-    else{
-        throw new Error('Erro ao criar um novo contato!')
-    }
+
+    const response = await fetch(url, options)
+
+    if(!response.ok) throw new Error('Erro ao criar um novo contato')
+        return response.json()
 }
