34b5d051bc3c1ffb940c1c2f0dc18ac863d13aff
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-05-26 16:34:56 -0300

    enviando o inserir funcionando

--- app.js
+++ app.js
@@ -10,3 +10,25 @@ const novoContato ={
     "endereco": "A única pessoa que nunca erra é a pessoa que não faz nada",
     "cidade": "Pense"
 }
+
+async function inserirContato(){
+    const inputNome = document.getElementById('nome').value
+    const inputCelular = document.getElementById('celular').value
+    const inputFoto = document.getElementById('foto').value
+    const inputEmail = document.getElementById('email').value
+    const inputEndereco = document.getElementById('endereco').value
+    const inputCidade = document.getElementById('cidade').value
+
+    const contato ={
+        inputNome,
+        inputCelular,
+        inputFoto,
+        inputEmail,
+        inputEndereco,
+        inputCidade
+    }
+
+    document.getElementById('bt_cadastro').addEventListener('click', await inserirContato(await postContato(contato)))
+}
+
+inserirContato()
